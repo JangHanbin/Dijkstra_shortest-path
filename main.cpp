3ad4c9331e94e89f74c03345a42292ac13232607
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2021-11-07 15:01:14 +0900

    made it

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <list>
+#include <queue>
+#include <ctime>
 
 using namespace std;
 
@@ -16,55 +17,43 @@ void print_array(int* buf, int size_of_buf)
 void explore_lowest_metric_node(int explorer_node[], int exploree_node[], int basement_metric, int number_of_nodes) //basement metric is metric value way to exploree node from explorer node
 {
     for (int i = 0; i < number_of_nodes; ++i) {
-        if(exploree_node[i] == 0 || explorer_node==0) //skip own node path
+        if(exploree_node[i] == 0 || explorer_node[i]==0) //skip own node path
             continue;
 
         int metric_to_path = exploree_node[i];
 
-        if(explorer_node[i] > exploree_node[i] + basement_metric || explorer_node[i]==-1) // if via metric value is less than  explorer's metric value, or unreachable node can be reacable,
+        if((explorer_node[i] > exploree_node[i] + basement_metric || explorer_node[i]==-1) && (exploree_node[i] !=-1 && explorer_node[i] !=0) ) // if via metric value is less than  explorer's metric value, or unreachable node can be reacable,
         {
-            exploree_node[i] = exploree_node[i] + basement_metric; // update metric
+            explorer_node[i] = exploree_node[i] + basement_metric; // update metric
         }
     }
 
 }
-void find_shortest(int buf[], int idx)
+
+int find_min_metric(int buf[], bool explored_metric_idx[], int number_of_nodes) //this fucntion uses selection sort algorithm. will be replaced other sorting algorithms (e.g., bubble,  merge sort, quick sort ...)
 {
-
-
-
-}
-
-void find_min_metric(int buf[], int min_metric_buf[], int number_of_nodes) //this fucntion uses selection sort algorithm. will be replaced other sorting algorithms (e.g., bubble,  merge sort, quick sort ...)
-{
-
     int min = INT_MAX;
     int min_idx = -1;
-    int idx = 0;
-    int tmp[number_of_nodes];
-    memcpy(tmp,buf, number_of_nodes * sizeof(int));
 
     for (int i = 0; i < number_of_nodes; ++i) {
-        for (int j = 0; j < number_of_nodes; ++j) {
-            if(min> tmp[j] )
-            {
-                min = tmp[j];
-                min_idx=j;
-            }
+        if(explored_metric_idx[i]==1) // if already explored in previous step
+            continue; //skip
 
+        if (buf[i] < min && (buf[i]!=0 && buf[i] !=-1)) //check metric value and unreachable or itself
+        {
+            min_idx = i; // save index of mininum metric
+            min = buf[i];
         }
-        min = INT_MAX;
-
-        min_metric_buf[idx++] = min_idx;
-        tmp[min_idx] = INT_MAX; // replace min value to INT_MAX value to avoid duplication.
-
 
     }
+
+    return min_idx;
+
 }
 int main()
 {
 
-    int nodes[][6] = {
+    int nodes[][6] = { //initalizing nodes metrics
         {0, 2, 5, 1, -1, -1},
         {2, 0, 3, 2, -1, -1},
         {5, 3, 0, 3, 1, 5},
@@ -75,34 +64,29 @@ int main()
     };
 
     int number_of_nodes = sqrt(sizeof(nodes)/sizeof(int));
-    int min_metric_buf[number_of_nodes];
-
-
-    memset(min_metric_buf, 0, number_of_nodes*sizeof(int));
-
-
-
-
-
-
 
 
     for (int i = 0; i < number_of_nodes ; ++i) {
-        find_min_metric(nodes[i], min_metric_buf, number_of_nodes); // find metrics by lowest.
+        bool explored_metric_idx[number_of_nodes];
+        memset(explored_metric_idx, false, number_of_nodes*sizeof(int));
 
-        for (int j = 0; j < number_of_nodes; ++j) {
-            if(nodes[i][min_metric_buf[j]] <=0) // if lowest metric is 0, -1 this is, node[i][x] is own self or unreachable
+        for (int j = 0; j < number_of_nodes -1 ; ++j) { // we don't need to explore own node. that's why set a number of interation count to number_of_nodes -1
+            int lowest_metric_idx = find_min_metric(nodes[i], explored_metric_idx, number_of_nodes); // find metrics by lowest.
+            if(lowest_metric_idx ==-1)
+            {
+                cout<<"There is unreachable Node"<<endl;
                 continue;
+            }
 
-            int lowest_metric_idx = min_metric_buf[j];
-            print_array(nodes[i], number_of_nodes);
             explore_lowest_metric_node(nodes[i], nodes[lowest_metric_idx], nodes[i][lowest_metric_idx], number_of_nodes); //explore lowest metric node
-            print_array(nodes[i], number_of_nodes);
+            explored_metric_idx[lowest_metric_idx] = true; //set flag to avoid duplicate node explore
 
-            cout<<"@@@@@@@@@@@@@@@@@@@@@"<<endl;
+
         }
-        cout<<endl;
 
+        cout<<"Optimized node["<<i<<"] : "<<endl;
+        print_array(nodes[i], number_of_nodes);
+        cout<<endl;
 
 
 
